ec6eadb5bd09840df7affca1ab7eaf5745642e49
wgumenyuk <wlad@gumenyuk.de>
2024-12-17 18:09:32 +0100

    feat: add `exclude` parameter

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -1,13 +1,13 @@
 import sys, csv, json
 
-def csv_to_json(csv_file_path: str, json_file_path: str) -> None:
+def csv_to_json(csv_file_path: str, json_file_path: str, exclude: list[str] = []) -> None:
 	rows = []
 
 	with open(csv_file_path) as csv_file:
 		csv_reader = csv.DictReader(csv_file)
 
 		for row in csv_reader:
-			rows.append(row)
+			rows.append({ key: value for key, value in row.items() if key not in exclude })
 
 	with open(json_file_path, "w") as json_file:
 		json_file.write(json.dumps(rows))
@@ -18,4 +18,4 @@ if __name__ == "__main__":
 	if len(args) < 2:
 		exit(1)
 
-	csv_to_json(args[0], args[1])
+	csv_to_json(args[0], args[1], args[2:])
