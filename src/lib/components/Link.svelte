29452c0f277fe72292eb714785ca9d919a6fd859
wgumenyuk <wlad@gumenyuk.de>
2024-12-23 00:11:03 +0100

    feat: add `Link` component

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { ExternalLinkIcon } from "lucide-svelte";
+
+	// Types
+	import type { Snippet } from "svelte";
+
+	let {
+		href,
+		children
+	}: {
+		href: string;
+		children: Snippet;
+	} = $props();
+</script>
+
+<a
+	{href}
+	class="flex w-fit border-spacing-2 items-center gap-1.5 border-b border-dotted border-slate-500"
+>
+	{@render children()}
+	<ExternalLinkIcon size="16px" />
+</a>
