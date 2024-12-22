e183488d32f5429aa556e64484a5d6bb70a6a630
wgumenyuk <wlad@gumenyuk.de>
2024-12-23 00:22:42 +0100

    feat: add `CompaniesTable` component

---
+++ src/lib/components/CompaniesTable.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Link from "$components/Link.svelte";
+	import Table from "$components/Table.svelte";
+
+	// Data
+	import data from "$lib/data/breaches_symbols.json";
+
+	const { format } = new Intl.NumberFormat("de-DE");
+</script>
+
+{#snippet row(item: (typeof data)[0])}
+	<div class="w-full">
+		<Link href="/what/{item.Symbol}">{item.Company}</Link>
+	</div>
+	<div class="w-full">
+		<Link href="/what/{item.Symbol}">{item.Symbol}</Link>
+	</div>
+	<span class="w-full">{item.Industry}</span>
+	<span class="w-full">{item.Date}</span>
+	<span class="w-full">{item.Type.join(", ")}</span>
+	<span class="w-full">
+		{!isNaN(parseInt(item.Affected))
+			? format(parseInt(item.Affected))
+			: item.Affected}
+	</span>
+{/snippet}
+
+<Table
+	columns={["Company", "Symbol", "Industry", "Date", "Type", "Affected"]}
+	{data}
+	{row}
+/>
