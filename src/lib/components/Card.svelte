ef2b362f2e1cc3448740c176e31ec961c85a2175
wgumenyuk <wlad@gumenyuk.de>
2024-12-23 17:48:23 +0100

    feat: accept optional classes

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
-	let { children } = $props();
+	import clsx from "clsx";
+
+	// Types
+	import type { Snippet } from "svelte";
+
+	let {
+		class: optionalClasses,
+		children
+	}: {
+		class?: string;
+		children: Snippet;
+	} = $props();
+
+	const cardClass = clsx(
+		optionalClasses,
+		"rounded-lg",
+		"bg-slate-50",
+		"p-8",
+		"shadow-sm"
+	);
 </script>
 
-<div class="rounded-lg bg-slate-50 p-4 shadow-sm">
+<div class={cardClass}>
 	{@render children()}
 </div>
