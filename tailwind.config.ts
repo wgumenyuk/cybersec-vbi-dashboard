59c5f17b589d0afb9598566459224a80862ac018
wgumenyuk <wlad@gumenyuk.de>
2024-12-26 00:42:26 +0100

    chore: add "silver" color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,24 @@ import type { Config } from "tailwindcss";
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	plugins: [typography],
+	darkMode: ["selector", `[data-theme="dark"]`],
 	theme: {
 		extend: {
+			colors: {
+				silver: {
+					50: "#F2F2F3",
+					100: "#E3E2E4",
+					200: "#C9C8CB",
+					300: "#ACACAF",
+					400: "#939296",
+					500: "#77767B",
+					600: "#605F63",
+					700: "#474649",
+					800: "#302F31",
+					900: "#171617",
+					950: "#0D0C0D"
+				}
+			},
 			fontFamily: {
 				sans: "Roboto, sans-serif"
 			}
