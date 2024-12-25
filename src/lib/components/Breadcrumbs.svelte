4b4be191daab7f65c19f8cbd75e7bc41ac11e1e7
wgumenyuk <wlad@gumenyuk.de>
2024-12-26 00:38:58 +0100

    feat: add `Breadcrumbs` component

---
+++ src/lib/components/Breadcrumbs.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { ChevronRightIcon } from "lucide-svelte";
+
+	type Node = {
+		label: string;
+		href?: string;
+	};
+
+	let {
+		nodes
+	}: {
+		nodes: Node[];
+	} = $props();
+</script>
+
+<div class="flex items-center gap-0.5">
+	{#each nodes as node, i}
+		{#if node.href}
+			<a href={node.href} class="text-silver-500 dark:text-silver-400"
+				>{node.label}</a
+			>
+		{:else}
+			<span class="text-silver-500 dark:text-silver-400"
+				>{node.label}</span
+			>
+		{/if}
+
+		{#if i < nodes.length - 1}
+			<ChevronRightIcon size="16px" />
+		{/if}
+	{/each}
+</div>
