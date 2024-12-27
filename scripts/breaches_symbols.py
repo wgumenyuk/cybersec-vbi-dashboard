653bd06383cc9c075733a765b7e6df09e4f49512
wgumenyuk <wlad@gumenyuk.de>
2024-12-27 22:28:16 +0100

    feat: add `breaches_symbols` data prep script

---
+++ scripts/breaches_symbols.py
@@ -0,0 +1,28 @@
+import csv, json, re
+
+csv_file_path = "data/original/breaches_symbols.csv"
+json_file_path = "data/transformed/breaches_symbols.json"
+
+with open(csv_file_path) as csv_file:
+	csv_reader = csv.DictReader(csv_file)
+	rows = []
+
+	for i, row in enumerate(csv_reader):
+		item = {
+			"ID": i + 1,
+			"Company": row["Company"],
+			"Symbol": row["Symbol"],
+			"Industry": row["Industry"],
+			"Date": row["Date"],
+			"Type": re.split(",\\s*", row["Type"])
+		}
+
+		try:
+			item["Affected"] = int(row["Affected"].replace(".", ""))
+		except ValueError:
+			item["Affected"] = "Unknown"
+
+		rows.append(item)
+
+	with open(json_file_path, "w") as json_file:
+		json_file.write(json.dumps(rows))
