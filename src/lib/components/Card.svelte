373a3d015e2c2587e20cd70eb2f97188a5fc16ce
wgumenyuk <wlad@gumenyuk.de>
2024-12-30 19:23:42 +0100

    refactor: drop `clsx`

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-	import clsx from "clsx";
-
-	// Types
 	import type { Snippet } from "svelte";
 
 	let {
 		children,
-		class: oc
+		class: c
 	}: {
 		children?: Snippet;
 		class?: string;
 	} = $props();
+</script>
 
-	const c = clsx(
-		oc,
+<div
+	class={[
 		"bg-silver-100",
 		"dark:bg-silver-900",
 		"p-4",
@@ -22,10 +20,9 @@
 		"border-silver-200",
 		"dark:border-silver-800",
 		"shadow-sm",
-		"dark:shadow-none"
-	);
-</script>
-
-<div class={c}>
+		"dark:shadow-none",
+		!!c && c
+	]}
+>
 	{@render children?.()}
 </div>
