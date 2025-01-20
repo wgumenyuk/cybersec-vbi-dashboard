4184ccf081c81abf8658f47610881e8f07f2a80a
wgumenyuk <wlad@gumenyuk.de>
2025-01-20 16:37:20 +0100

    chore: calculate `Type` cardinality

--- scripts/breaches_symbols_meta.py
+++ scripts/breaches_symbols_meta.py
@@ -1,3 +1,4 @@
+import itertools
 import pandas as pd
 import numpy as np
 
@@ -7,13 +8,16 @@ df = pd.DataFrame(pd.read_json(dataset_path))
 c_company = df["Company"].nunique()
 c_industry = df["Industry"].nunique()
 
+types = list(itertools.chain(*df["Type"]))
+c_types = len(set(types))
+
 r_affected = np.ptp(df.loc[df["Affected"] != "Unknown", ["Affected"]])
 r_pre = np.ptp(df.loc[~(df["Pre"].isnull()), ["Pre"]])
 r_during = np.ptp(df.loc[~(df["During"].isnull()), ["During"]])
 r_post = np.ptp(df.loc[~(df["Post"].isnull()), ["Post"]])
 r_stock_change = np.ptp(df.loc[~(df["Stock Change"].isnull()), ["Stock Change"]])
 
-print(f"{c_company=} {c_industry=}")
+print(f"{c_company=} {c_industry=} {c_types=}")
 print(f"{r_affected=}")
 print(f"{r_pre=} {r_during=} {r_post=}")
 print(f"{r_stock_change=}")
